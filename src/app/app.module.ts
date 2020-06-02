37d11d34183f14ae98fddd1f8ced9cd3c4adbe38
Osvaldo Castellanos <ocstllns@gmail.com>
2020-06-02 14:14:52 -0500

    adding initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { DashviewComponent } from './dashboard/dashview/dashview.component';
+import { SystemsListComponent } from './systems/systems-list/systems-list.component';
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TodoListComponent,
+    DashviewComponent,
+    SystemsListComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/systems/systems-list/systems-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-systems-list',
+  templateUrl: './systems-list.component.html',
+  styleUrls: ['./systems-list.component.css']
+})
+export class SystemsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
